4cb78264664a39e4ff7ad8f8c684d41cee60367f
benjaminroesdal <53909855+benjaminroesdal@users.noreply.github.com>
2022-12-01 11:05:31 +0000

    Deploying to gh-pages from  @ f3b06023b8c3f31526570ff42dc284664fed2690 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -61,7 +61,7 @@
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-sqGn3xx5WWnNKC\/Nc20bnU4\/cDH\/ZwiLSk7tZY\/r5ZA=",
+      "hash": "sha256-vUcwc+vsPjSwE2sT\/WMuxbTGZpaHwOz\/rc+2ZV5Py+g=",
       "url": "index.html"
     },
     {
@@ -365,5 +365,5 @@
       "url": "_content\/MudBlazor\/MudBlazor.min.js"
     }
   ],
-  "version": "amAIAqSU"
+  "version": "mL\/\/Hxsa"
 };
